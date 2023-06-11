c63152f14f30052f9a7c6ccd3c5c49ca78c860f4
Aleksandr Shirkhanyan <97232437+ashirkhanyan@users.noreply.github.com>
2023-06-11 14:35:58 +0400

    Making it flexible for our laptops too

--- code/config.py
+++ code/config.py
@@ -1,3 +1,4 @@
+import os
 
 SEED = 42
 LOSS = "ce"  # ce [Cross Entropy], hb [Huberloss], l1 [MAE]
@@ -7,8 +8,16 @@ MOMENTUM = 0.9
 MODEL = "resnet" # resnet, densenet, mobilenet
 BATCH_SIZE = 32
 EPOCHS = 30
-DATA_FOLDER = "/Users/srv/Documents/Cloud/Georgia Institute of Technology/MVP - General/Ultrasound-labeled"
-PLOT_FOLDER = "/Users/srv/Documents/Cloud/Georgia Institute of Technology/MVP - General/plots"
+
+if os.environ.get('USERNAME') == 'srv':
+    fld = "/Users/srv/Documents/Cloud"
+else:
+    fld = "/Users/aleks"
+
+DATA_FOLDER = fld + "/Georgia Institute of Technology/MVP - General/Ultrasound-labeled"
+PLOT_FOLDER = fld + "/Georgia Institute of Technology/MVP - General/plots"
+
+
 TRAIN_PART = 0.9
 PATIENCE = 2
 REDUCE_FACTOR = 0.5
