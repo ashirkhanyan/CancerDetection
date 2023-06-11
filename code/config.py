233e122092f99f59e897e3d4192bd9b4bea09055
Saurav Suman <saurav.prakash.suman@outlook.in>
2023-06-11 17:11:17 +0400

    finished gradcam

--- code/config.py
+++ code/config.py
@@ -2,15 +2,25 @@
 SEED = 42
 LOSS = "ce"  # ce [Cross Entropy], hb [Huberloss], l1 [MAE]
 OPTIMIZER = "sgd" # sgd, adam
-LEARNING_RATE = 0.1
+LEARNING_RATE = 0.01
 MOMENTUM = 0.9
 MODEL = "resnet" # resnet, densenet, mobilenet
 BATCH_SIZE = 32
+VIS_BATCH_SIZE = 7
 EPOCHS = 30
 DATA_FOLDER = "/Users/srv/Documents/Cloud/Georgia Institute of Technology/MVP - General/Ultrasound-labeled"
 PLOT_FOLDER = "/Users/srv/Documents/Cloud/Georgia Institute of Technology/MVP - General/plots"
 TRAIN_PART = 0.9
-PATIENCE = 2
-REDUCE_FACTOR = 0.5
+PATIENCE = 1
+REDUCE_FACTOR = 0.1
 IMAGE_WIDTH = 768
 IMAGE_HEIGHT = 576
+VISUALIZER = "gdcam" #GradCAM
+CLASS_MAP = {
+    "benign": 0,
+    "malignant": 1,
+}
+INV_CLASS_MAP = {
+    0: "benign",
+    1: "malignant",
+}
