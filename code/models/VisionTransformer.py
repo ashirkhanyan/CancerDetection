26bc240d4b5d7900e1de45cc054f3e477b6bee1a
Aleksandr Shirkhanyan <97232437+ashirkhanyan@users.noreply.github.com>
2023-06-17 18:16:01 +0400

    Update VisionTransformer.py

--- code/models/VisionTransformer.py
+++ code/models/VisionTransformer.py
@@ -11,21 +11,12 @@ class VisionTransformer(nn.Module):
         self.classifier = nn.Linear(self.model.config.hidden_size, 2)
 
 
-
     def forward(self, x):
-        batch_size, num_channels, height, width = x.shape
-        x = x.view(batch_size, num_channels, height * width)  # Flatten height and width dimensions
-        x = x.transpose(1, 2)  # Swap dimensions to match DeiT model's input expectations
+        x = self.resize(x)
         outputs = self.model(x)
         logits = self.classifier(outputs.last_hidden_state[:, 0])
         return logits
 
 
 
-# =============================================================================
-#     def forward(self, x):
-#         x = self.resize(x)
-#         outputs = self.model(x)
-#         logits = self.classifier(outputs.last_hidden_state[:, 0])
-#         return logits
-# =============================================================================
+
