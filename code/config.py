09a92f26d4a354caffb35ffb82b2c6756b12c5cd
ssuman34 <saurav.suman@gatech.edu>
2023-07-11 19:18:09 +0400

    final changes

--- code/config.py
+++ code/config.py
@@ -5,17 +5,17 @@ LOSS = "fl"  # ce [Cross Entropy], hb [Huberloss], l1 [MAE]
 OPTIMIZER = "sgd" # sgd, adam
 LEARNING_RATE = 0.01
 MOMENTUM = 0.9
-MODEL = "ssd" # resnet, densenet, mobilenet, transformer, fasterrcnn, ssd
+MODEL = "fasterrcnn" # resnet, densenet, mobilenet, transformer, fasterrcnn, ssd
 MODEL_TYPE = "obj_detection"  # obj_detection, classification
 MODEL_BACKBONE = "resnet"   # resnet, mobilenet
 BOX_SHAPE = "xyxy" # xyxy, xywh
-BATCH_SIZE = 64
+BATCH_SIZE = 8
 
 VIS_BATCH_SIZE = 0
 VIS_BOUND_BOX = True
 
 
-EPOCHS = 30
+EPOCHS = 20
 
 VIS_MODEL_WEIGHTS = "1_resnet_ce_sgd_0.01_0.1_30"
 VIS_MODEL = "resnet"
